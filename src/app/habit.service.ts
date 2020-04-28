99679076279a560621b7ac120bcedf93c315c059
Sam Julien <spjulien@gmail.com>
2020-04-27 18:15:39 -0700

    Add count

--- src/app/habit.service.ts
+++ src/app/habit.service.ts
@@ -9,18 +9,22 @@ export class HabitService {
     {
       id: 1,
       title: 'Check in with parents once a week',
+      count: 5,
     },
     {
       id: 2,
       title: 'Record 2 videos per day',
+      count: 4,
     },
     {
       id: 3,
       title: 'Work on side project 5 hours/week',
+      count: 3,
     },
     {
       id: 4,
       title: 'Write for 20 minutes a day',
+      count: 6,
     },
   ];
 
