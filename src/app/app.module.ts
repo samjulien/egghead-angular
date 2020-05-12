c95b794912a57387db56a5d5adc287c33936a0fe
Sam Julien <spjulien@gmail.com>
2020-05-12 09:02:03 -0700

    Use server and add proxy config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HabitListComponent } from './habit-list/habit-list.component';
@@ -7,13 +8,13 @@ import { HabitItemComponent } from './habit-item/habit-item.component';
 import { HabitFormComponent } from './habit-form/habit-form.component';
 
 @NgModule({
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   declarations: [
     AppComponent,
     HabitListComponent,
     HabitItemComponent,
     HabitFormComponent,
   ],
-  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/habit.service.ts
+++ src/app/habit.service.ts
@@ -1,38 +1,18 @@
 import { Injectable } from '@angular/core';
 import { of, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Habit } from './habit';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HabitService {
-  habits: Habit[] = [
-    {
-      id: 1,
-      title: 'Check in with parents once a week',
-      count: 5,
-    },
-    {
-      id: 2,
-      title: 'Record 2 videos per day',
-      count: 4,
-    },
-    {
-      id: 3,
-      title: 'Work on side project 5 hours/week',
-      count: 3,
-    },
-    {
-      id: 4,
-      title: 'Write for 20 minutes a day',
-      count: 6,
-    },
-  ];
+  habits: Habit[];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   getHabits(): Observable<Habit[]> {
-    return of(this.habits);
+    return this.http.get<Habit[]>('/api/habits');
   }
 
   addHabit(newHabit) {
